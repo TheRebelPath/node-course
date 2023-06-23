6fc18f0552ffa8a1393a7a172f9caec1d3070675
Rodrigo Yanez <wintermute.ryf@gmail.com>
2023-06-23 02:35:54 -0600

    Add more reference examples

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const Person = require('./person');
+// const Person = require('./person');
 
+// const person1 = new Person('bob sponge', 30);
 
-const person1 = new Person('bob sponge', 30);
+// person1.greeting();
+const Logger = require("./logger");
 
-person1.greeting();
+const logger = new Logger();
+
+logger.on("message", (data) => console.log('Called Listener', data));
+
+logger.log("Hello There");
